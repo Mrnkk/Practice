bd8c534116eab805afaf6422dc6a19f0648c271f
Mrnkk <122083285+Mrnkk@users.noreply.github.com>
2025-03-03 17:23:40 +0530

    Add files via upload

---
+++ Practice/Practice/src/com/temp/Temp1.java
@@ -0,0 +1,17 @@
+package com.temp;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Temp1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack s = new Stack();
+		Queue q = new LinkedList<Integer>();
+		
+		
+	}
+
+}
